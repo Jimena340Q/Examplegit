7853bca8ed321c73d9e0688a8a8d5d27b5b3d806
ximena quintana <quintanaximena340@gmail.com>
2023-09-18 10:40:31 -0500

    Creación de archivo Example1.py
    Se crea ejemplo de números aleatorios en Python.

---
+++ Example_python/Example1.py
@@ -0,0 +1,28 @@
+"""
+    Fecha:18/09/2023
+    Autor: Ximena Quintana
+    Objetivo: Ejemplo de versionamiento con got desde python
+"""
+import random
+
+random_number = random.randint(1, 10) #Genera un numero aleatrotio entre (randint)
+print(random_number)
+
+print("----------------------------------------------------------------")
+
+for i in range(0, 10):
+    random_number = random.randrange(20, 100, 5)#Genera numeros mutiplos de el tercer numero (randrange)
+    print(random_number)
+
+print("----------------------------------------------------------------")
+
+for i in range(0, 10):
+    random_number = random.random() #random.random : numeros decimales 
+    print(random_number)
+
+print("----------------------------------------------------------------")
+
+for i in range(0, 10):
+    random_number = random.uniform(10.5, 20.6) # random.uniform : numeros decimales de un rango ()
+    print(random_number)
+
